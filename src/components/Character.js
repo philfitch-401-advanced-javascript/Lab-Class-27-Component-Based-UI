c22e7f04ee083d9d62b952df43ba6e7b9d18b1c7
Phil Fitch <mostreluctantly@gmail.com>
2019-10-30 17:05:48 -0700

    refactored Character

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Character.css';
 
-const Character = ({ image, name, species }) => {
-  return (
-    <dl className={styles.Character}>
-      <img src={image} />
-      <div>
-        <dt>Name</dt>
-        <dd>{name}</dd>
+// const Character = ({ image, name, species }) => {
+//   return (
+//     <dl className={styles.Character}>
+//       <img src={image} />
+//       <div>
+//         <dt>Name</dt>
+//         <dd>{name}</dd>
 
-        <dt>Species</dt>
-        <dd>{species}</dd>
-      </div>
-    </dl>
-  );
-};
+//         <dt>Species</dt>
+//         <dd>{species}</dd>
+//       </div>
+//     </dl>
+//   );
+// };
 
-Character.propTypes = {
-  image: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  species: PropTypes.string.isRequired
-};
+// Character.propTypes = {
+//   image: PropTypes.string.isRequired,
+//   name: PropTypes.string.isRequired,
+//   species: PropTypes.string.isRequired
+// };
+
+class Character extends Component {
+  static propTypes = {
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    species: PropTypes.string.isRequired
+  }
+
+  render() {
+    return (
+      <dl className={styles.Character}>
+        <img src={this.props.image} />
+        <div>
+          <dt>Name</dt>
+          <dd>{this.props.name}</dd>
+
+          <dt>Species</dt>
+          <dd>{this.props.species}</dd>
+        </div>
+      </dl>
+    );
+  }
+}
+
 
 export default Character;
